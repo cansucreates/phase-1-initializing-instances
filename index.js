e6fe7fe77b31d3698c23cdf73760b750cd43d7b2
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-04-03 16:59:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+
+class Breakfast{
+    constructor(food,drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+const breakfast = new Breakfast("cereal","milk");
+
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+const lunch = new Lunch("salad","tomato soup","orange juice");
+
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+        
+    }
+}
+
+const dinner = new Dinner("mediterian salad","fish soup","meat","tiramisu");
